0ff9305baaa30e86ddf33dc08feb3e7288553dde
Emanuele <egalli64@gmail.com>
2025-07-28 09:50:39 +0200

    minimal test

---
+++ src/test/java/com/example/sec/SpringSecurityApplicationTests.java
@@ -0,0 +1,16 @@
+/*
+ * Spring Boot Security tutorial 
+ * 
+ * https://github.com/egalli64/spring-security
+ */
+package com.example.sec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
