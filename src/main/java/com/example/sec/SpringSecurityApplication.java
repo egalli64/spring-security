e74454165172e9ace6b3975fef467738a816d359
Emanuele <egalli64@gmail.com>
2025-07-28 09:40:45 +0200

    plain main

---
+++ src/main/java/com/example/sec/SpringSecurityApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Spring Boot Security tutorial 
+ * 
+ * https://github.com/egalli64/spring-security
+ */
+package com.example.sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+}
