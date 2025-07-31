e5df88ba78b9db0a5e1810d4b46b74e04285af34
Emanuele <egalli64@gmail.com>
2025-07-31 11:56:05 +0200

    login endpoint

--- src/main/java/com/example/sec/AppController.java
+++ src/main/java/com/example/sec/AppController.java
@@ -5,20 +5,29 @@
  */
 package com.example.sec;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AppController {
+    private static final Logger log = LogManager.getLogger(AppController.class);
+
     @GetMapping("/")
     public String home(Model model) {
+        log.traceEntry();
+
         model.addAttribute("message", "Welcome to Spring Security Tutorial!");
         return "home";
     }
 
     @GetMapping("/public")
     public String publicEndpoint(Model model) {
+        log.traceEntry();
+
         model.addAttribute("title", "A public page");
         model.addAttribute("content", "This page should be accessible to everyone");
 
@@ -27,6 +36,8 @@ public class AppController {
 
     @GetMapping("/private")
     public String privateEndpoint(Model model) {
+        log.traceEntry();
+
         model.addAttribute("title", "A private page");
         model.addAttribute("content", "This page should require authentication");
 
@@ -35,9 +46,25 @@ public class AppController {
 
     @GetMapping("/admin")
     public String adminEndpoint(Model model) {
+        log.traceEntry();
+
         model.addAttribute("title", "An admin page");
         model.addAttribute("content", "This page should require admin role");
 
         return "simple";
+    }
+
+    /**
+     * Custom login page
+     */
+    @GetMapping("/login")
+    public String login(@RequestParam(required = false) String error, Model model) {
+        log.traceEntry("error is {}", error);
+
+        if (error != null) {
+            model.addAttribute("errorMessage", "Invalid username or password");
+        }
+
+        return "login";
     }
 }
