8aeacaef42de2961c872b4eefd70a7a50d2f0836
Emanuele <egalli64@gmail.com>
2025-08-05 17:09:04 +0200

    JPA entity authority repository

---
+++ src/main/java/com/example/sec/SecAuthorityRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Spring Boot Security tutorial 
+ * 
+ * https://github.com/egalli64/spring-security
+ */
+package com.example.sec;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SecAuthorityRepository extends JpaRepository<SecRole, Long> {
+    Optional<SecAuthority> findByName(String name);
+
+    boolean existsByName(String name);
+}
